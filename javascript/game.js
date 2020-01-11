993a98907346dd01ba29ec7f8145486f5ad7c891
Carolyn Grady <owenv8@gmail.com>
2020-01-10 19:17:24 -0500

    started the document.ready function

--- javascript/game.js
+++ javascript/game.js
@@ -58,17 +58,49 @@ function fighterValues() {
 
 // create fighter tokens using jQuery's and append to stage-one's fighters div
 // chosen fighter and fighter index are passed in parameters
-function fighterToken(index, fighter){
+function fighterToken(index, fighter) {
 
     // class is given to the div since there are multiple tokens
     let tokenDiv = $('<div class="token">');
 
     // create token elements to go into the div
-    let tokenImg = $('<img alt="head shot"').attr('src', // figure out how to call the image here);
+    let tokenImg = $('<img alt="head shot"').attr('src', /*figure out how to call the image here*/);
     let tokenHP = $('<div class="token-label">').text(fightersArray.health); //calling the array here isnt bringing up the text
     let tokenName = $('<div class="token-label">').text(fightersArray.health);// figure out how to call the array info from here
 
     // append token elements to tokenDiv
     tokenDiv.append(tokenName).append(tokenImg).append(tokenHP);
 }
+
+
+
+
+
+
+
+// start game after page loads
+$(document).ready(function(){
+
+    //click event to select fighter
+    //of all the fighters, clicking on a fighter's token selects the fighter
+    $('#allFighters').click('.token', function(){
+        let fighter = $(this);
+
+        // move the the fighter seleceted by the click event to the userFighter ID div
+        $('#userFighter').append(this);
+
+        // move unselected fighters to stage-two div
+    })
+
+    // click event for the attack button
+
+    // click event for the strong attack button
+
+    // click event for reset button
+
+
+
+
+
+})
 
